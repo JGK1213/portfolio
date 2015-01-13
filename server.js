4f5fb33d7044a061e356e97830ff92508e31843d
JGK1213 <jackiegwk@gmail.com>
2015-01-13 12:54:54 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+// server.js
+
+    // set up ========================
+    var express  = require('express');
+    /////// library to use with node app
+    var app      = express();                               // create our app w/ express
+    var mongoose = require('mongoose');                     // mongoose for mongodb
+    var morgan = require('morgan');             // log requests to the console (express4)
+    var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+    var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+
+    // configuration =================
+
+    mongoose.connect('mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
+
+    app.use(express.static(__dirname + '/app'));                 // set the static files location /public/img will be /img for users
+    app.use(morgan('dev'));                                         // log every request to the console
+    app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+    app.use(bodyParser.json());                                     // parse application/json
+    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+    app.use(methodOverride());
+
+    // listen (start app with node server.js) ======================================
+		/////// environment in production or development, change it from the server 
+		///// default to 3000 since we are not setting the port
+		var port = process.env.PORT || 3000;
+
+		//// listen to the var port
+		app.listen(port);
+
+		//// cool, the server is running
+		console.log('server running on port ' + port);
+
+
